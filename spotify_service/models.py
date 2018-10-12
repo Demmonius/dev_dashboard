6263a7d56ae9e36d17902f53d684061537aafc37
Hugo MARTIN <hugo.martin@epitech.eu>
2018-10-12 22:43:11 +0000

    Fixed spotify player to be compliant

--- spotify_service/models.py
+++ spotify_service/models.py
@@ -9,8 +9,16 @@ class SpotifyPlayer(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def get(self, instance):
+		url = ""
+		if self.url.find("spotify:") >= 0:
+			url = self.url.split(':')[1:]
+			url = '/'.join(url)
+		elif self.url.find("https://open.spotify.com/") >= 0:
+			print(self.url)
+			url = "https://open.spotify.com/embed/" + self.url.replace('https://open.spotify.com/', '')
+			print(url)
 		return {
-			'id': self.url.split('/')[-1]
+			'url': url
 		}
 
 	def __str__(self):
