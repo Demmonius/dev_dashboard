fda445693b8b1a0891481a65699bdf1c124334ca
Hugo MARTIN <hugo.martin@epitech.eu>
2018-10-15 21:38:19 +0000

    Re-updated settings.py

--- code/dashboard/settings.py
+++ code/dashboard/settings.py
@@ -18,8 +18,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-django_heroku.settings(locals())
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -160,3 +158,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'login/static'), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/signin'
+
+django_heroku.settings(locals())
