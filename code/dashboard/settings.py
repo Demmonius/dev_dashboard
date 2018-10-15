015a3c0e804f751d667ad9bf0f757416888f85ff
Hugo MARTIN <hugo.martin@epitech.eu>
2018-10-15 21:43:36 +0000

    Remvoed useless css

--- code/dashboard/settings.py
+++ code/dashboard/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y%6(!qxjufwwn%g5#@@wozanzlnrx=a=0*+%)45c!3#m55zu!k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
